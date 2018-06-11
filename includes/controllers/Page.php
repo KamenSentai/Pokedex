d1382cac0c42826fab778c6a8bb2f30160c9b417
AlainKRSS <cvt.alain@gmail.com>
2018-06-11 03:11:47 +0200

    🎮 controllers > Refactor code

--- includes/controllers/Page.php
+++ includes/controllers/Page.php
@@ -64,9 +64,73 @@ class Page
      * @param object $response
      * @return object $this->container->view->render()
      */
+    public function getPokemon(Request $request, Response $response, $arguments)
+    {
+        // Get pokemons data
+        $dataView     = $this->container->getPokemons;
+        $dataBase     = $dataView['base'];
+        $dataData     = $dataView['data'];
+        $slug         = $arguments['slug'];
+        $pokemonIndex = array_search($slug, array_column($dataData['pokemons'], 'slug'));
+
+        // Exception
+        if (!$pokemonIndex && !is_int($pokemonIndex))
+            throw new \Slim\Exception\NotFoundException($request, $response);
+        else
+            $pokemon = $dataData['pokemons'][$pokemonIndex];
+
+        // Set new data
+        $dataBase['page']    = 'pokemon';
+        $dataBase['title']   = TITLE . ' | ' . (isset($pokemon->is_owned) ? $pokemon->name : '???');
+        $dataData['pokemon'] = $pokemon;
+        $dataView['base']    = $dataBase;
+        $dataView['data']    = $dataData;
+
+        return $this->container->view->render($response, 'index.twig', $dataView);
+    }
+
+    /**
+     * @param object $request
+     * @param object $response
+     * @return object $this->container->view->render()
+     */
     public function getTypes(Request $request, Response $response)
     {
         return $this->container->view->render($response, 'index.twig', $this->container->getTypes);
+    }
+
+    /**
+     * @param object $request
+     * @param object $response
+     * @return object $this->container->view->render()
+     */
+    public function getType(Request $request, Response $response, $arguments)
+    {
+        // Get pokemons data
+        $dataView = $this->container->getPokemons;
+        $dataBase = $dataView['base'];
+        $dataData = $dataView['data'];
+        $pokemons = $dataData['pokemons'];
+        $slug     = $arguments['slug'];
+
+        // Exception
+        if (!in_array(ucfirst($slug), $this->container->getTypes['data']['types']))
+            throw new \Slim\Exception\NotFoundException($request, $response);
+
+        // Remove unused pokemons
+        foreach ($pokemons as $_pokemon)
+            if (!in_array(ucfirst($slug), $_pokemon->type))
+                unset($pokemons[array_search($_pokemon, $pokemons)]);
+
+        // Set new data
+        $dataBase['page']     = 'type';
+        $dataBase['title']    = TITLE . ' | ' . ucfirst($slug);
+        $dataData['pokemons'] = $pokemons;
+        $dataData['type']     = $slug;
+        $dataView['base']     = $dataBase;
+        $dataView['data']     = $dataData;
+
+        return $this->container->view->render($response, 'index.twig', $dataView);
     }
 
     /**
--- includes/routes/routing.php
+++ includes/routes/routing.php
@@ -18,75 +18,13 @@ $app->get('/catch', PC\Page::class . ':getCatch')->setName('catch');
 $app->get('/pokemons', PC\Page::class . ':getPokemons')->setName('pokemons');
 
 // Pokemon
-$app
-    ->get(
-        '/pokemons/{slug:[a-z_]+}',
-        function(Request $request, Response $response, $arguments)
-        {
-            // Get pokemons data
-            $dataView     = $this->getPokemons;
-            $dataBase     = $dataView['base'];
-            $dataData     = $dataView['data'];
-            $slug         = $arguments['slug'];
-            $pokemonIndex = array_search($slug, array_column($dataData['pokemons'], 'slug'));
-
-            // Exception
-            if (!$pokemonIndex && !is_int($pokemonIndex))
-                throw new \Slim\Exception\NotFoundException($request, $response);
-            else
-                $pokemon = $dataData['pokemons'][$pokemonIndex];
-
-            // Set new data
-            $dataBase['page']    = 'pokemon';
-            $dataBase['title']   = TITLE . ' | ' . (isset($pokemon->is_owned) ? $pokemon->name : '???');
-            $dataData['pokemon'] = $pokemon;
-            $dataView['base']    = $dataBase;
-            $dataView['data']    = $dataData;
-
-            return $this->view->render($response, 'index.twig', $dataView);
-        }
-    )
-    ->setName('pokemon')
-;
+$app->get('/pokemons/{slug:[a-z_]+}', PC\Page::class . ':getPokemon')->setName('pokemon');
 
 // Types
 $app->get('/types', PC\Page::class . ':getTypes')->setName('types');
 
 // Type
-$app
-    ->get(
-        '/types/{slug:[a-z]+}',
-        function(Request $request, Response $response, $arguments)
-        {
-            // Get pokemons data
-            $dataView = $this->getPokemons;
-            $dataBase = $dataView['base'];
-            $dataData = $dataView['data'];
-            $pokemons = $dataData['pokemons'];
-            $slug     = $arguments['slug'];
-
-            // Exception
-            if (!in_array(ucfirst($slug), $this->getTypes['data']['types']))
-                throw new \Slim\Exception\NotFoundException($request, $response);
-
-            // Remove unused pokemons
-            foreach ($pokemons as $_pokemon)
-                if (!in_array(ucfirst($slug), $_pokemon->type))
-                    unset($pokemons[array_search($_pokemon, $pokemons)]);
-
-            // Set new data
-            $dataBase['page']     = 'type';
-            $dataBase['title']    = TITLE . ' | ' . ucfirst($slug);
-            $dataData['pokemons'] = $pokemons;
-            $dataData['type']     = $slug;
-            $dataView['base']     = $dataBase;
-            $dataView['data']     = $dataData;
-
-            return $this->view->render($response, 'index.twig', $dataView);
-        }
-    )
-    ->setName('type')
-;
+$app->get('/types/{slug:[a-z]+}', PC\Page::class . ':getType')->setName('type');
 
 // Random
 $app->get('/random', PC\Page::class . ':getRandom')->setName('random');
